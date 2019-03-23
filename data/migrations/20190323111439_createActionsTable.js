ab93668e4d848d6eb1acefa58854027d64825259
CatsAreMLG <joshuadgon@gmail.com>
2019-03-23 11:42:09 -0500

    Change actions_description to action_description

--- data/migrations/20190323111439_createActionsTable.js
+++ data/migrations/20190323111439_createActionsTable.js
@@ -2,7 +2,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('actions', tbl => {
     tbl.increments()
     tbl
-      .text('actions_description')
+      .text('action_description')
       .notNullable()
       .unique()
     tbl
