70f2cade56821b33ecd2319d283ea83d14d682ce
CatsAreMLG <joshuadgon@gmail.com>
2019-03-23 11:40:01 -0500

    Change table_name to project_name

--- data/migrations/20190323111255_createProjectsTable.js
+++ data/migrations/20190323111255_createProjectsTable.js
@@ -2,7 +2,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('projects', tbl => {
     tbl.increments()
     tbl
-      .string('table_name', 128)
+      .string('project_name', 128)
       .notNullable()
       .unique()
     tbl
