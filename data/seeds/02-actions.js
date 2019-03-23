11c78517dbf96bf984095530d04032d42393a735
CatsAreMLG <joshuadgon@gmail.com>
2019-03-23 11:35:38 -0500

    Add actions seed

---
+++ data/seeds/02-actions.js
@@ -0,0 +1,30 @@
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('table_name')
+    .del()
+    .then(function() {
+      // Inserts seed entries
+      return knex('table_name').insert([
+        {
+          action_description: 'clean room',
+          notes: 'wipe down surfaces, sweep floor, etc.',
+          completed: false
+        },
+        {
+          action_description: 'get produce',
+          notes: 'veggies',
+          completed: false
+        },
+        {
+          action_description: 'get meat',
+          notes: 'meat, chicken, fish, etc.',
+          completed: false
+        },
+        {
+          action_description: 'pay phone bill',
+          notes: 'pay phone bill: $50',
+          completed: false
+        }
+      ])
+    })
+}
